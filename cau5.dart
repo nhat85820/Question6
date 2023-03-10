53d39edf2bca0893dc3d1277e55827720ebcaad9
nhat85820 <101626673+nhat85820@users.noreply.github.com>
2023-03-10 21:19:08 +0700

    Update cau5.dart

--- cau5.dart
+++ cau5.dart
@@ -1,34 +1,33 @@
-class Camera {
-  int _id;
-  String _brand;
-  String _color;
-  double _prize;
 
-  Camera(this._id, this._brand, this._color, this._prize);
 
-  int get id => _id;
-  set id(int value) => _id = value;
+class Camera{
+  String? _id;
+  String? _brand;
+  String? _prize;
 
-  String get brand => _brand;
-  set brand(String value) => _brand = value;
+  Camera(this._id, this._brand, this._prize);
 
-  String get color => _color;
-  set color(String value) => _color = value;
+  String get id => this._id!;
+  String get brand => this._brand!;
+  String get prize =>this._prize!;
 
-  double get prize => _prize;
-  set prize(double value) => _prize = value;
+  set id(String id) => this._id=id;
+  set name(String brand) =>this._brand=brand;
+  set prize(String prize) => this._prize=prize;
+
+  void display(){
+    print("ID: ${_id}");
+    print("Name: ${_brand}");
+    print("Prize: ${_prize}");
+  }
 }
+void main(List<String> args) {
+  for(int i=1; i<=3; i++){
+    Camera cm=Camera("ID0$i", "Brand0$i", "Prize0$i");
+    cm.display();
+    print("----------------------");
+  }
+  
+  
 
-void main() {
-  Camera cam1 = Camera(1, "Canon", "Black", 2000.0);
-  Camera cam2 = Camera(2, "Nikon", "Red", 2500.0);
-  Camera cam3 = Camera(3, "Sony", "Silver", 3000.0);
-
-  cam1.prize = 2200.0;
-  cam2.color = "Blue";
-  cam3.brand = "Fujifilm";
-
-  print("Camera 1 - ID: ${cam1.id}, Brand: ${cam1.brand}, Color: ${cam1.color}, Prize: ${cam1.prize}");
-  print("Camera 2 - ID: ${cam2.id}, Brand: ${cam2.brand}, Color: ${cam2.color}, Prize: ${cam2.prize}");
-  print("Camera 3 - ID: ${cam3.id}, Brand: ${cam3.brand}, Color: ${cam3.color}, Prize: ${cam3.prize}");
 }
